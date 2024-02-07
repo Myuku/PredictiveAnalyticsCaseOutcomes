037317e91707ab275592022ba1ad8a8131dbd02e
Myuku <Junelee2002jh@gmail.com>
2024-02-06 16:02:23 -0800

    uploaded base files #2

---
+++ main.py
@@ -0,0 +1,16 @@
+import sklearn as sl
+import pandas as pd
+import numpy as np
+import matplotlib as plot
+import seaborn as sb
+
+import data_cleaning as cd
+import data_processing as dp
+import data_visualization as dv
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
