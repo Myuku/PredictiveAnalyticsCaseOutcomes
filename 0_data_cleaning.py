67006be5fe80e1606c1beb5dab7ec84918a2e5fd
Alisa Koh <alisakoh8@gmail.com>
2024-03-16 11:55:40 -0700

    small clean up and added mapping catergorical to numerical for outcome feature

--- 0_data_cleaning.py
+++ 0_data_cleaning.py
@@ -1,4 +1,3 @@
-import sys
 import pandas as pd
 
 def read_data(path):
--- 1_data_processing.py
+++ 1_data_processing.py
@@ -7,7 +7,7 @@ def read_data(path):
 def main():
     location_data = read_data('./clean_data/location_2021.csv')
     train_cases = read_data('./clean_data/cases_2021_train.csv')
-    # test_cases = read_data('./clean_data/cases_2021_test.csv')
+    test_cases = read_data('./clean_data/cases_2021_test.csv')
     
     # Merge data
     merged_data = pd.merge(train_cases, location_data, how='left', on=['country', 'province'])        # Error note: some province are NaN in location_2021.csv so this is not working currently
@@ -18,8 +18,19 @@ def main():
     merged_data = pd.merge(merged_data, observed_deaths_data, how='left', on=['country', 'province']) 
     merged_data['Expected_Mortality_Rate'] = merged_data['observed_deaths'] / merged_data['Deaths']
     
+    # Map categorical features to numeric
+    merged_data.loc[~merged_data['outcome'].isin(['Deceased', 'Hospitalized']), 'outcome'] = 'Non_hospitalized'
+    merged_data['outcome'].replace(to_replace=['Deceased', 'Hospitalized', 'Non_hospitalized'], value=[0, 1, 2], inplace=True)
+    print('merged_data: \n', merged_data)
+    
+    test_cases.loc[~test_cases['outcome'].isin(['Deceased', 'Hospitalized']), 'outcome'] = 'Non_hospitalized'
+    test_cases['outcome'].replace(to_replace=['Deceased', 'Hospitalized', 'Non_hospitalized'], value=[0, 1, 2], inplace=True)
+    
+    # ... add on
+    
     # Write out as clean data file
     merged_data.to_csv('./clean_data/merged_data.csv', index=False)
+    test_cases.to_csv('./clean_data/test_cases.csv', index=False)
     return
     
 if __name__ == '__main__':
